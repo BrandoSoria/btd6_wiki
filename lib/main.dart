a06e1a562a2c5d517bdde6d59a2d49f7572b548e
brandon <webarebrandy@gmail.com>
2023-12-02 19:30:14 -0600

    acomodo del main al multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,53 @@
+
+import 'package:configuracion_loggin/providers/btd6_provider.dart';
 import 'package:configuracion_loggin/screens/odyssey_information_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 
-void main() => runApp(MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Btd6Provider(),
+          lazy: false,
+        ),
+        // ChangeNotifierProvider(
+        //   create: (_) => LoginFormProvider(),
+        //   lazy: false,
+        // ),
+        // ChangeNotifierProvider(
+        //   create: (_) => AuthService(),
+        //   lazy: false,
+        // )
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('BTD6 Odyssey Info'),
-        ),
-        body: OdysseyDetails(odyssey: getSampleOdyssey()), // Pasa la odisea como argumento
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'BTD6 Information Odyssey Atributes',
+      // initialRoute: 'login',
+      routes: {
+        // 'login': (_) => LoginScreen(),
+        // 'register': (_) => RegisterScreen(),
+        'home': (_) => OdysseyDetails(),
+        
+        // 'checking': (_) => CheckAuthScreen()
+      },
     );
   }
 }
