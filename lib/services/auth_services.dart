7c8b4417083de9391b327aab87f37626e0024631
brandon <webarebrandy@gmail.com>
2023-12-11 20:13:14 -0600

    auth_services lectura de añadir leer y eliminar jefes favoritos

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -4,16 +4,12 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
-  // Solicitudes HTTP
   final String _baseUrl = 'http://www.jwtingesoria.somee.com';
+  final String _apiBaseUrl = 'https://mis-jefes-btd6.onrender.com/api';
   final storage = FlutterSecureStorage();
 
   Future<String?> createUser(String email, String password) async {
-    final authData = {
-      'email': email,
-      'password': password,
-    };
-
+    final authData = {'email': email, 'password': password};
     final url = Uri.parse('$_baseUrl/api/Cuentas/registrar');
 
     try {
@@ -22,8 +18,6 @@ class AuthService extends ChangeNotifier {
         headers: {"Content-Type": "application/json"},
         body: json.encode(authData),
       );
-
-      print('Respuesta del servidor: ${resp.body}');
 
       if (resp.statusCode == 200) {
         final Map<String, dynamic> decodedResp = json.decode(resp.body);
@@ -44,11 +38,7 @@ class AuthService extends ChangeNotifier {
   }
 
   Future<String?> login(String email, String password) async {
-    final authData = {
-      'email': email,
-      'password': password,
-    };
-
+    final authData = {'email': email, 'password': password};
     final url = Uri.parse('$_baseUrl/api/Cuentas/Login');
 
     try {
@@ -57,8 +47,6 @@ class AuthService extends ChangeNotifier {
         headers: {"Content-Type": "application/json"},
         body: json.encode(authData),
       );
-
-      print('Respuesta del servidor: ${resp.body}');
 
       if (resp.statusCode == 200) {
         final Map<String, dynamic> decodedResp = json.decode(resp.body);
@@ -78,9 +66,75 @@ class AuthService extends ChangeNotifier {
     }
   }
 
+  Future<List<Map<String, dynamic>>> getFavoriteBosses() async {
+    final token = await readToken();
+    final response = await http.get(
+      Uri.parse('$_apiBaseUrl/favoritos'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.cast<Map<String, dynamic>>();
+    } else {
+      throw Exception(
+          'Error al obtener jefes favoritos: ${response.reasonPhrase}');
+    }
+  }
+
+  Future<void> addFavoriteBoss(String nombreJefe, String imagen) async {
+    final token = await readToken();
+    final headers = {
+      'Authorization': 'Bearer $token',
+      'Content-Type': 'application/json',
+    };
+
+    final body = json.encode({
+      'nombreJefe': nombreJefe,
+      'imagen': imagen,
+    });
+
+    try {
+      final response = await http.post(
+        Uri.parse('$_apiBaseUrl/favoritos/agregar'),
+        headers: headers,
+        body: body,
+      );
+
+      if (response.statusCode == 201) {
+        print('Jefe favorito agregado correctamente');
+      } else {
+        throw Exception(
+            'Error al agregar jefe favorito: ${response.reasonPhrase}');
+      }
+    } catch (error) {
+      throw Exception('Error al agregar jefe favorito: $error');
+    }
+  }
+
+  Future<void> removeFavoriteBoss(String idJefe) async {
+    final token = await readToken();
+    final headers = {'Authorization': 'Bearer $token'};
+
+    try {
+      final response = await http.delete(
+        Uri.parse('$_apiBaseUrl/favoritos/quitar/$idJefe'),
+        headers: headers,
+      );
+
+      if (response.statusCode == 200) {
+        print('Jefe favorito eliminado correctamente');
+      } else {
+        throw Exception(
+            'Error al quitar jefe favorito: ${response.reasonPhrase}');
+      }
+    } catch (error) {
+      throw Exception('Error al quitar jefe favorito: $error');
+    }
+  }
+
   Future logout() async {
     await storage.delete(key: 'token');
-    return;
   }
 
   Future<String> readToken() async {
